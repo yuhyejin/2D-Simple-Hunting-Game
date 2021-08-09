f592d97e8b8e4603fb8d395fdb67815b11831ea6
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-08-09 15:51:56 +0900

    유니티 플랫폼 게임
    유니티 점프 플랫폼 게임 제작.
    
    * 2021. 08. 08. 일요일
    @ 타일맵 이용해서 플랫폼 만들기
    @ 캐릭터 애니메이션 적용
    @ 캐릭터 이동 및 점프 기능

---
+++ Unity2DPlatformGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Jump
+        if (Input.GetButton("Jump"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, maxSpeed * jumpPower);
+            anim.SetBool("isJumping", true);
+        }
+
+        //Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        //Direction Sprite
+        if(Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        //Animation
+        if (rigid.velocity.normalized.x == 0)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+    }
+
+    void FixedUpdate()
+    {
+        //Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(maxSpeed * h, rigid.velocity.y);
+        //rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //Max Speed
+        if (rigid.velocity.x > maxSpeed)    //Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if (rigid.velocity.x < maxSpeed * (-1))    //Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        //Landing Platform
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1);
+        if(rayHit.collider != null)
+        {
+            Debug.Log(rayHit.collider.name);
+        }
+    }
+}
