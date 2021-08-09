f4e6aa9a39f1edd0cb3a5b4800727856dfc247e4
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-08-10 01:23:49 +0900

    유니티 플랫폼 게임 4일차
    * 2021. 08. 09. 월요일
    @ 아이템 기능 넣기 (절반 실패)
    @ 적(쥐) 애니메이션 넣고 이동기능

---
+++ Unity2DPlatformGame/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(-1, rigid.velocity.y);
+
+    }
+}
