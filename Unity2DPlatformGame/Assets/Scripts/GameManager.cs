019b94e031d5da042342992a3b0a6409575d1f1e
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-08-11 23:47:50 +0900

    아이템 기능
    * 2021. 08. 11. 수요일
    @ 적 AI 이동 기능 넣기 실패
    @ 아이템 기능 넣기 (아이템 먹으면 사라지고, 점수 올라감 다이아몬드 아이템 추가)

---
+++ Unity2DPlatformGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
