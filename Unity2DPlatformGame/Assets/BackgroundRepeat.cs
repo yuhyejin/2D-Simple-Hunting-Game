40a407ed4c4738b99d553f35c76c276ab865e887
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2021-08-17 21:59:12 +0900

    무한 배경
    *2021. 08. 17. 화요일
    @ 무한 배경을 만들려했지만 뭐때문인지 실패함...ㅠ

---
+++ Unity2DPlatformGame/Assets/BackgroundRepeat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public const float backSpeed = 3.0f;
+    private Material thisMaterial;
+
+    void Start()
+    {
+        thisMaterial = GetComponent<Renderer>().material;
+    }
+
+
+    void Update()
+    {
+        float newOffsetX = thisMaterial.mainTextureOffset.x + backSpeed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(newOffsetX, 0);
+        thisMaterial.mainTextureOffset = newOffset;
+    }
+}
